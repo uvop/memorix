f4c531a1496a7e4dd421b4d4c63ae0aa1cb50abd
Omer Prizner <omer.prizner@gmail.com>
2022-04-28 20:01:38 +0300

    BaseMemorix CacheItem implementation

--- clients/js/src/BaseMemorixApi.ts
+++ clients/js/src/BaseMemorixApi.ts
@@ -5,15 +5,17 @@ import { CacheItem, GlobalCacheItem } from "./types";
 export class BaseMemorixApi {
   getCacheItem<Payload>(identifier: string): GlobalCacheItem<Payload>;
   getCacheItem<Key, Payload>(identifier: string): CacheItem<Key, Payload>;
-  getCacheItem(identifier: string) {
+  getCacheItem<Key, Payload>(identifier: string): any {
     console.log(identifier);
 
     return {
-      set(key, payload) {
-        console.log(key, payload);
+      set(...args: [Key, Payload] | [Payload]) {
+        const key = args.length === 1 ? undefined : args[0];
+        const payload = args.length === 1 ? args[0] : args[1];
+
         return Promise.resolve();
       },
-      get(key) {
+      get(key?: Key) {
         console.log(key);
         return Promise.resolve();
       },
