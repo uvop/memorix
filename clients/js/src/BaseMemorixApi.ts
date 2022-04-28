b77d2a7cf115c7ced0aedeadfa194fc8ca868f4d
AmirAltman <006amir@gmail.com>
2022-04-28 17:31:47 +0000

    added get and set cache implementation

--- clients/js/src/BaseMemorixApi.ts
+++ clients/js/src/BaseMemorixApi.ts
@@ -1,23 +1,36 @@
 /* eslint-disable class-methods-use-this */
+import Redis from "ioredis";
 
 import { CacheItem, GlobalCacheItem } from "./types";
 
 export class BaseMemorixApi {
+  redis: Redis;
+
+  constructor() {
+    this.redis = new Redis(process.env.REDIS_ENV as string);
+  }
+
   getCacheItem<Payload>(identifier: string): GlobalCacheItem<Payload>;
   getCacheItem<Key, Payload>(identifier: string): CacheItem<Key, Payload>;
   getCacheItem<Key, Payload>(identifier: string): any {
     console.log(identifier);
 
     return {
-      set(...args: [Key, Payload] | [Payload]) {
+      set: async (...args: [Key, Payload] | [Payload]) => {
         const key = args.length === 1 ? undefined : args[0];
         const payload = args.length === 1 ? args[0] : args[1];
-
+        const hashedKey = key ? identifier + key : identifier;
+        await this.redis.set(hashedKey, JSON.stringify(payload));
         return Promise.resolve();
       },
-      get(key?: Key) {
+      get: async (key?: Key) => {
         console.log(key);
-        return Promise.resolve();
+        const hashedKey = key ? identifier + key : identifier;
+        const found = await this.redis.get(hashedKey);
+        if (found) {
+          return JSON.parse(found);
+        }
+        return null;
       },
     };
   }
--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -1,3 +1,14 @@
 import { BaseMemorixApi } from "./BaseMemorixApi";
 
+const init = async () => {
+  const api = new BaseMemorixApi();
+  const { get, set } = api.getCacheItem<string, string>("memo");
+  set("key", "success");
+
+  const data = await get("key");
+  console.log(data);
+};
+
+init();
+
 export { BaseMemorixApi };
