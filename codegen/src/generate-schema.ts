0336a29d7d0345cfa9f35586dee61ce49c449dd5
Yuval Saraf <unimonkiez@gmail.com>
2022-04-28 19:22:26 +0300

    Fix generate from schema if outout not given

--- codegen/src/generate-schema.ts
+++ codegen/src/generate-schema.ts
@@ -7,12 +7,22 @@ const [, , schemaArgPath, outputPath] = process.argv;
 const generateSchema = async () => {
   const schemaPath = path.resolve(schemaArgPath);
 
-  const isOutputPathADir = path.extname(outputPath).length === 0;
-  const codeDir = isOutputPathADir ? outputPath : path.dirname(outputPath);
-  const codeFilename = isOutputPathADir
-    ? `${path.basename(schemaPath, path.extname(schemaPath))}.generated.ts`
-    : path.basename(outputPath);
+  const isOutputPathDefined = !!outputPath;
+  const isOutputPathADir =
+    isOutputPathDefined && path.extname(outputPath).length === 0;
+
+  // eslint-disable-next-line no-nested-ternary
+  const codeDir = isOutputPathDefined
+    ? isOutputPathADir
+      ? outputPath
+      : path.dirname(outputPath)
+    : path.dirname(schemaPath);
+  const codeFilename =
+    isOutputPathADir || !isOutputPathDefined
+      ? `${path.basename(schemaPath, path.extname(schemaPath))}.generated.ts`
+      : path.basename(outputPath);
   const codePath = path.join(codeDir, codeFilename);
+
   const schema = await (await fs.promises.readFile(schemaPath)).toString();
 
   const code = codegen({ schema });
