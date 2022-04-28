3ddd9907419c82364893eac1264bd18ae4653833
Omer Prizner <omer.prizner@gmail.com>
2022-04-28 21:36:50 +0300

    polish cache handling memorix

--- clients/js/src/BaseMemorixApi.ts
+++ clients/js/src/BaseMemorixApi.ts
@@ -1,36 +1,27 @@
-/* eslint-disable class-methods-use-this */
 import Redis from "ioredis";
-
 import { CacheItem } from "./types";
 import { hashKey } from "./utils/hashKey";
 
 export class BaseMemorixApi {
-  redis: Redis;
-
-  constructor() {
-    this.redis = new Redis(process.env.REDIS_ENV as string);
-  }
+  readonly redis = new Redis(process.env.REDIS_ENV!);
 
   getCacheItem<Key, Payload>(identifier: string): CacheItem<Key, Payload> {
-    const hashCacheKey = (key: Key | undefined) =>
-      hashKey(key ? [identifier, key] : [identifier]);
+    const hashCacheKey = (key: Key | undefined) => {
+      return hashKey(key ? [identifier, key] : [identifier]);
+    };
 
     return {
       set: async (...args) => {
         const key = args.length === 1 ? undefined : args[0];
         const payload = args.length === 1 ? args[0] : args[1];
-
         const hashedKey = hashCacheKey(key);
-
         await this.redis.set(hashedKey, JSON.stringify(payload));
-        return Promise.resolve();
       },
       get: async (...[key]) => {
         const hashedKey = hashCacheKey(key);
-
         const found = await this.redis.get(hashedKey);
         if (found) {
-          return JSON.parse(found);
+          return JSON.parse(found) as Payload;
         }
         return null;
       },
--- clients/js/src/types.ts
+++ clients/js/src/types.ts
@@ -1,5 +1,7 @@
 export type CacheItem<Key, Payload> = {
-  get(...args: Key extends undefined ? [] : [key: Key]): Promise<Payload>;
+  get(
+    ...args: Key extends undefined ? [] : [key: Key]
+  ): Promise<Payload | null>;
   set(
     ...args: Key extends undefined
       ? [payload: Payload]
