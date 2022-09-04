e60babbdc7f49d20b694ba0a377fd478b518433a
Yuval Saraf <unimonkiez@gmail.com>
2022-09-05 00:57:38 +0300

    Try fix ci

--- clients/redis/js/src/example-schema.generated.ts
+++ clients/redis/js/src/example-schema.generated.ts
@@ -16,9 +16,11 @@ export class MemorixApi extends MemorixClientApi {
     favoriteAnimal: this.getCacheItem<string, Animal>("favoriteAnimal"),
     user: this.getCacheItem<string, User>("user"),
   };
+
   pubsub = {
     message: this.getPubsubItem<undefined, string>("message"),
   };
+
   task = {
     runAlgo: this.getTaskItem<undefined, string, Animal>("runAlgo"),
   };
