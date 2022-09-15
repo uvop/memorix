5a5787212df3a2caba66add534b2b4c9489a19e0
Yuval Saraf <unimonkiez@gmail.com>
2022-09-15 22:48:16 +0300

    Try publish to homebrew

---
+++ scripts/formula_template.rb
@@ -0,0 +1,19 @@
+cask "memorix" do
+    version "{{{VERSION}}}"
+  
+    if Hardware::CPU.intel?
+        url "https://github.com/uvop/memorix/releases/download/v#{version}/memorix-macos-x64.tar.gz"
+        sha256 "{{{INTEL_SHA}}}"
+    else
+        url "https://github.com/uvop/memorix/releases/download/v#{version}/memorix-macos-x64.tar.gz"
+        sha256 "{{{M1_SHA}}}"
+    end
+  
+    name "Memorix"
+    desc "Memorix - CLI"
+    homepage "https://github.com/uvop/memorix"
+  
+    def install
+        bin.install "memorix"
+    end
+  end
---
+++ scripts/publish_homebrew_version.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$VERSION" ]]; then
+  echo "Missing target VERSION environment variable, please specify it"
+  exit 1
+fi
+
+# Removing the v suffix, if present
+VERSION=${VERSION#"v"}
+
+
+echo "Targeting version $VERSION"
+
+echo "Reading artifacts hashes"
+INTEL_SHA=$(shasum -a 256 ./cli/lib/release/memorix-macos-x64.tar.gz)
+# M1_SHA=$(shasum -a 256 ./cli/lib/release/memorix-macos-arm64.tar.gz)
+M1_SHA=$INTEL_SHA
+
+echo "Cloning tap repository"
+
+git clone git@github.com:uvop/homebrew-memorix.git
+echo "Rendering formula template"
+
+cat ../scripts/formula_template.rb | sed "s/{{{VERSION}}}/$VERSION/g" | \
+  sed "s/{{{INTEL_SHA}}}/$INTEL_SHA/g" | sed "s/{{{M1_SHA}}}/$M1_SHA/g" > ./Formula/memorix.rb
+
+echo "Committing version update"
+git add Formula/memorix.rb
+git commit -m "Version bump: $VERSION"
+
+echo "Pushing changes"
+git push
+
+echo "Done!"
