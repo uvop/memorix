1a7f3bb0ddf3e388b4ba15b246889c4942ed0a93
Yuval Saraf <unimonkiez@gmail.com>
2022-09-16 13:07:05 +0300

    Copied template

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ nvm use
 
 yarn
 
-for appPath in ./cli ./clients/js ./clients/python ./app/backend ./app/frontend; do
+for appPath in ./docs ./cli ./clients/js ./clients/python ./app/backend ./app/frontend; do
     if [ -f $appPath/install.sh ]; then
         echo "running install for $appPath"
         (cd $appPath && ./install.sh)
