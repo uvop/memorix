f3e19f287bc2e9e575adeafaa6374648ea31779d
Yuval Saraf <unimonkiez@gmail.com>
2023-11-06 18:43:17 +0200

    Try fix

--- clients/redis/rust/tests/test.rs
+++ clients/redis/rust/tests/test.rs
@@ -5,7 +5,7 @@ mod example_schema_generated;
 
 async fn get_memorix() -> Result<example_schema_generated::Memorix, Box<dyn std::error::Error>> {
     let redis_url =
-        std::env::var("RUST_REDIS_URL").expect("missing environment variable RUST_REDIS_URL");
+        std::env::var("REDIS_URL").expect("missing environment variable REDIS_URL");
     let memorix = example_schema_generated::Memorix::new(&redis_url).await?;
     Ok(memorix)
 }
