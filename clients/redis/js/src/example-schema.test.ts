ac050f91f3a02bee50f7daa37a83a5f9eba990d4
Yuval Saraf <unimonkiez@gmail.com>
2024-10-16 19:53:16 +0300

    Converted tests, task funky

--- clients/redis/js/src/example-schema.test.ts
+++ clients/redis/js/src/example-schema.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "jsr:@std/assert";
+import { assertEquals, assertNotEquals } from "jsr:@std/assert";
 import { Animal, Memorix } from "./example-schema.generated.ts";
 // import { afterEach, beforeEach, describe, it } from "jsr:@std/testing/bdd";
 // import { expect } from "jsr:@std/expect";
@@ -12,7 +12,7 @@ Deno.test("simple cache", async () => {
   const user = await memorix.cache.user.get("uv");
   assertEquals(user!.age, 29);
 });
-Deno.test("simple cache", async () => {
+Deno.test.ignore("simple cache expire", async () => {
   await memorix.cache.user.set("uv", { name: "uv", age: 29 });
   const user1 = await memorix.cache.user.get("uv");
   assertEquals(user1!.age, 29);
@@ -21,234 +21,267 @@ Deno.test("simple cache", async () => {
   assertEquals(user2, null);
 });
 
-//   it("cache expire", async () => {
-//     await memorix.cache.user.set(
-//       "uv",
-//       { name: "uv", age: 29 },
-//       { expire: { value: 1 } },
-//     );
-//     const user1 = await memorix.cache.user.get("uv");
-//     expect(user1!.age).toBe(29);
-//     await new Promise((res) => setTimeout(res, 1500));
-//     const user2 = await memorix.cache.user.get("uv");
-//     expect(user2).toBe(null);
-//   });
-//   it("cache expire in schema", async () => {
-//     await memorix.cache.userExpire.set("uv", { name: "uv", age: 29 });
-//     const user1 = await memorix.cache.userExpire.get("uv");
-//     expect(user1!.age).toBe(29);
-//     await new Promise((res) => setTimeout(res, 1500));
-//     const user2 = await memorix.cache.userExpire.get("uv");
-//     expect(user2).toBe(null);
-//   });
-//   it("cache expire none", async () => {
-//     await memorix.cache.userExpire2.set("uv", { name: "uv", age: 29 });
-//     await new Promise((res) => setTimeout(res, 2500));
-//     const user = await memorix.cache.userExpire2.get("uv");
-//     expect(user).not.toBe(null);
-//   });
-//   it("cache expire extending on get", async () => {
-//     await memorix.cache.userExpire3.set("uv", { name: "uv", age: 29 });
-//     await new Promise((res) => setTimeout(res, 1500));
-//     await memorix.cache.userExpire3.get("uv");
-//     await new Promise((res) => setTimeout(res, 1500));
-//     const user = await memorix.cache.userExpire3.get("uv");
-//     expect(user).not.toBe(null);
-//   });
-//   it("cache expire extending manually", async () => {
-//     await memorix.cache.userNoKey.set({ name: "uv", age: 29 });
-//     await new Promise((res) => setTimeout(res, 1500));
-//     await memorix.cache.userNoKey.get();
-//     await memorix.cache.userNoKey.extend();
-//     await new Promise((res) => setTimeout(res, 1500));
-//     const user = await memorix.cache.userNoKey.get();
-//     expect(user).not.toBe(null);
-//   });
-//   describe("pubsub", () => {
-//     // it("publish says how many subscribers", (done) => {
-//     //   memorix.pubsub.message.subscribe().then(() => {
-//     //     memorix.pubsub.message
-//     //       .publish("hello uv")
-//     //       .then(({ subscribersSize }) => {
-//     //         try {
-//     //           expect(subscribersSize).toBe(1);
-//     //           done();
-//     //         } catch (error) {
-//     //           done(error);
-//     //         }
-//     //       });
-//     //   });
-//     // });
-//     // it("subscribe gets payload", (done) => {
-//     //   let unsubscribe = (() => Promise.reject()) as () => Promise<void>;
-//     //   memorix.pubsub.message
-//     //     .subscribe((payload) => {
-//     //       try {
-//     //         expect(payload).toBe("hello uv");
-//     //         unsubscribe()
-//     //           .then(() => {
-//     //             done();
-//     //           })
-//     //           .catch((err) => {
-//     //             done(err);
-//     //           });
-//     //       } catch (error) {
-//     //         unsubscribe().finally(() => {
-//     //           done(error);
-//     //         });
-//     //       }
-//     //     })
-//     //     .then(({ unsubscribe: x }) => {
-//     //       unsubscribe = x;
-//     //     })
-//     //     .then(() => {
-//     //       memorix.pubsub.message.publish("hello uv");
-//     //     });
-//     // });
-//     it("subscribe gets payload with AsyncIterableIterator", async () => {
-//       setTimeout(() => {
-//         memorix.pubsub.message.publish("hello uv");
-//       }, 500);
-//       const subscription = await memorix.pubsub.message.subscribe();
-//       for await (const payload of subscription.asyncIterator) {
-//         expect(payload).toBe("hello uv");
-//         break;
-//       }
-//       await subscription.unsubscribe();
-//     });
-//   });
-//   describe("task", () => {
-//     beforeEach(async () => {
-//       await memorix.task.runAlgo.clear();
-//       await memorix.task.runAlgoNewest.clear();
-//     });
-//     it("queue returns the queue size", async () => {
-//       await memorix.task.runAlgo.queue("uv1");
-//       const { queueSize } = await memorix.task.runAlgo.queue("uv2");
-//       expect(queueSize).toBe(2);
-//     });
-//     it("queue clears correctly", async () => {
-//       await memorix.task.runAlgo.queue("uv1");
-//       const { queueSize } = await memorix.task.runAlgo.queue("uv2");
-//       expect(queueSize).toBe(2);
-//       await memorix.task.runAlgo.clear();
-//       const { queueSize: queueSizeAfterClear } = await memorix.task.runAlgo
-//         .queue("uv2");
-//       expect(queueSizeAfterClear).toBe(1);
-//     });
-//     // it("dequeue receives a message", (done) => {
-//     //   memorix.task.runAlgo
-//     //     .queue("uv3")
-//     //     .then(() => memorix.task.runAlgo.queue("uv4"))
-//     //     .then(({ queueSize }) => {
-//     //       expect(queueSize).toBe(2);
+Deno.test.ignore("cache expire", async () => {
+  await memorix.cache.user.set(
+    "uv",
+    { name: "uv", age: 29 },
+    { expire: { value: 1 } },
+  );
+  const user1 = await memorix.cache.user.get("uv");
+  assertEquals(user1!.age, 29);
+  await new Promise((res) => setTimeout(res, 1500));
+  const user2 = await memorix.cache.user.get("uv");
+  assertEquals(user2, null);
+});
+Deno.test.ignore("cache expire in schema", async () => {
+  await memorix.cache.userExpire.set("uv", { name: "uv", age: 29 });
+  const user1 = await memorix.cache.userExpire.get("uv");
+  assertEquals(user1!.age, 29);
+  await new Promise((res) => setTimeout(res, 1500));
+  const user2 = await memorix.cache.userExpire.get("uv");
+  assertEquals(user2, null);
+});
+Deno.test.ignore("cache expire none", async () => {
+  await memorix.cache.userExpire2.set("uv", { name: "uv", age: 29 });
+  await new Promise((res) => setTimeout(res, 2500));
+  const user = await memorix.cache.userExpire2.get("uv");
+  assertNotEquals(user, null);
+});
+Deno.test.ignore("cache expire extending on get", async () => {
+  await memorix.cache.userExpire3.set("uv", { name: "uv", age: 29 });
+  await new Promise((res) => setTimeout(res, 1500));
+  await memorix.cache.userExpire3.get("uv");
+  await new Promise((res) => setTimeout(res, 1500));
+  const user = await memorix.cache.userExpire3.get("uv");
+  assertNotEquals(user, null);
+});
+Deno.test.ignore("cache expire extending manually", async () => {
+  await memorix.cache.userNoKey.set({ name: "uv", age: 29 });
+  await new Promise((res) => setTimeout(res, 1500));
+  await memorix.cache.userNoKey.get();
+  await memorix.cache.userNoKey.extend();
+  await new Promise((res) => setTimeout(res, 1500));
+  const user = await memorix.cache.userNoKey.get();
+  assertNotEquals(user, null);
+});
+Deno.test(
+  "publish says how many subscribers",
+  () =>
+    new Promise((res, rej) => {
+      memorix.pubsub.message.subscribe().then(() => {
+        memorix.pubsub.message
+          .publish("hello uv")
+          .then(({ subscribersSize }) => {
+            try {
+              assertEquals(subscribersSize, 1);
+              res();
+            } catch (error) {
+              rej(error);
+            }
+          });
+      });
+    }),
+);
+Deno.test(
+  "publish says how many subscribers",
+  () =>
+    new Promise((res, rej) => {
+      let unsubscribe = (() => Promise.reject()) as () => Promise<void>;
+      memorix.pubsub.message
+        .subscribe((payload) => {
+          try {
+            assertEquals(payload, "hello uv");
+            unsubscribe()
+              .then(() => {
+                res();
+              })
+              .catch((err) => {
+                rej(err);
+              });
+          } catch (error) {
+            unsubscribe().finally(() => {
+              rej(error);
+            });
+          }
+        })
+        .then(({ unsubscribe: x }) => {
+          unsubscribe = x;
+        })
+        .then(() => {
+          memorix.pubsub.message.publish("hello uv");
+        });
+    }),
+);
+Deno.test("subscribe gets payload with AsyncIterableIterator", async () => {
+  setTimeout(() => {
+    memorix.pubsub.message.publish("hello uv");
+  }, 500);
+  const subscription = await memorix.pubsub.message.subscribe();
+  for await (const payload of subscription.asyncIterator) {
+    assertEquals(payload, "hello uv");
+    break;
+  }
+  await subscription.unsubscribe();
+});
 
-//     //       return new Promise((res) => {
-//     //         const payloads: string[] = [];
-//     //         let stop: any;
-//     //         memorix.task.runAlgo
-//     //           .dequeue((payload) => {
-//     //             payloads.push(payload);
-//     //             if (payloads.length === 2) {
-//     //               res({
-//     //                 payloads,
-//     //                 stop,
-//     //               });
-//     //             }
-//     //             return Animal.cat;
-//     //           })
-//     //           .then((args) => {
-//     //             stop = args.stop;
-//     //           });
-//     //       });
-//     //     })
-//     //     .then(async ({ payloads, stop }: any) => {
-//     //       expect(payloads).toStrictEqual(["uv3", "uv4"]);
-//     //       await stop();
-//     //       done();
-//     //     })
-//     //     .catch(done);
-//     // });
-//     // it("dequeue receives a message in opposite order if asked", (done) => {
-//     //   memorix.task.runAlgo
-//     //     .queue("uv6")
-//     //     .then(() => memorix.task.runAlgo.queue("uv7"))
-//     //     .then(({ queueSize }) => {
-//     //       expect(queueSize).toBe(2);
+const beforeTask = async () => {
+  await memorix.task.runAlgo.clear();
+  await memorix.task.runAlgoNewest.clear();
+};
 
-//     //       return new Promise((res) => {
-//     //         const payloads: string[] = [];
-//     //         let stop: any;
-//     //         memorix.task.runAlgo
-//     //           .dequeue(
-//     //             (payload) => {
-//     //               payloads.push(payload);
-//     //               if (payloads.length === 2) {
-//     //                 res({
-//     //                   payloads,
-//     //                   stop,
-//     //                 });
-//     //               }
-//     //               return Animal.cat;
-//     //             },
-//     //             { takeNewest: true },
-//     //           )
-//     //           .then((args) => {
-//     //             stop = args.stop;
-//     //           });
-//     //       });
-//     //     })
-//     //     .then(async ({ payloads, stop }: any) => {
-//     //       expect(payloads).toStrictEqual(["uv7", "uv6"]);
-//     //       await stop();
-//     //       done();
-//     //     })
-//     //     .catch(done);
-//     // });
-//     // it("dequeue receives a message in opposite order if asked from schema", (done) => {
-//     //   memorix.task.runAlgoNewest
-//     //     .queue("uv6")
-//     //     .then(() => memorix.task.runAlgoNewest.queue("uv7"))
-//     //     .then(({ queueSize }) => {
-//     //       expect(queueSize).toBe(2);
+Deno.test("queue returns the queue size", async () => {
+  await beforeTask();
+  await memorix.task.runAlgo.queue("uv1");
+  const { queueSize } = await memorix.task.runAlgo.queue("uv2");
+  assertEquals(queueSize, 2);
+});
+Deno.test("queue clears correctly", async () => {
+  await beforeTask();
+  await memorix.task.runAlgo.queue("uv1");
+  const { queueSize } = await memorix.task.runAlgo.queue("uv2");
+  assertEquals(queueSize, 2);
+  await memorix.task.runAlgo.clear();
+  const { queueSize: queueSizeAfterClear } = await memorix.task.runAlgo
+    .queue("uv2");
+  assertEquals(queueSizeAfterClear, 1);
+});
+Deno.test("queue clears correctly", async () => {
+  await beforeTask();
+  await memorix.task.runAlgo.queue("uv1");
+  const { queueSize } = await memorix.task.runAlgo.queue("uv2");
+  assertEquals(queueSize, 2);
+  await memorix.task.runAlgo.clear();
+  const { queueSize: queueSizeAfterClear } = await memorix.task.runAlgo
+    .queue("uv2");
+  assertEquals(queueSizeAfterClear, 1);
+});
+Deno.test("dequeue receives a message", async () => {
+  await beforeTask();
+  const p = new Promise((res, rej) => {
+    memorix.task.runAlgo
+      .queue("uv3")
+      .then(() => memorix.task.runAlgo.queue("uv4"))
+      .then(({ queueSize }) => {
+        assertEquals(queueSize, 2);
 
-//     //       return new Promise((res) => {
-//     //         const payloads: string[] = [];
-//     //         let stop: any;
-//     //         memorix.task.runAlgoNewest
-//     //           .dequeue((payload) => {
-//     //             payloads.push(payload);
-//     //             if (payloads.length === 2) {
-//     //               res({
-//     //                 payloads,
-//     //                 stop,
-//     //               });
-//     //             }
-//     //             return Animal.cat;
-//     //           })
-//     //           .then((args) => {
-//     //             stop = args.stop;
-//     //           });
-//     //       });
-//     //     })
-//     //     .then(async ({ payloads, stop }: any) => {
-//     //       expect(payloads).toStrictEqual(["uv7", "uv6"]);
-//     //       await stop();
-//     //       done();
-//     //     })
-//     //     .catch(done);
-//     // });
-//   });
-//   describe("namespace", () => {
-//     it("works for cache item", async () => {
-//       await memorix.spaceship.cache.pilot.set({ name: "uv" });
-//       const pilot = await memorix.spaceship.cache.pilot.get();
-//       expect(pilot!.name).toBe("uv");
-//     });
-//     it("works for recursive cache item", async () => {
-//       await memorix.spaceship.crew.cache.count.set(10);
-//       const count = await memorix.spaceship.crew.cache.count.get();
-//       expect(count).toBe(10);
-//     });
-//   });
-// });
+        return new Promise((res) => {
+          const payloads: string[] = [];
+          let stop: any;
+          memorix.task.runAlgo
+            .dequeue((payload) => {
+              payloads.push(payload);
+              if (payloads.length === 2) {
+                res({
+                  payloads,
+                  stop,
+                });
+              }
+              return Animal.cat;
+            })
+            .then((args) => {
+              stop = args.stop;
+            });
+        });
+      })
+      .then(async ({ payloads, stop }: any) => {
+        assertEquals(payloads, ["uv3", "uv4"]);
+        await stop();
+        res(undefined);
+      })
+      .catch(rej);
+  });
+  await p;
+});
+Deno.test(
+  "dequeue receives a message in opposite order if asked",
+  async () => {
+    await beforeTask();
+    const p = new Promise((res, rej) => {
+      memorix.task.runAlgo
+        .queue("uv6")
+        .then(() => memorix.task.runAlgo.queue("uv7"))
+        .then(({ queueSize }) => {
+          assertEquals(queueSize, 2);
+
+          return new Promise((res) => {
+            const payloads: string[] = [];
+            let stop: any;
+            memorix.task.runAlgo
+              .dequeue(
+                (payload) => {
+                  payloads.push(payload);
+                  if (payloads.length === 2) {
+                    res({
+                      payloads,
+                      stop,
+                    });
+                  }
+                  return Animal.cat;
+                },
+                { takeNewest: true },
+              )
+              .then((args) => {
+                stop = args.stop;
+              });
+          });
+        })
+        .then(async ({ payloads, stop }: any) => {
+          assertEquals(payloads, ["uv7", "uv6"]);
+          await stop();
+          res(undefined);
+        })
+        .catch(rej);
+    });
+    await p;
+  },
+);
+Deno.test(
+  "dequeue receives a message in opposite order if asked from schema",
+  async () => {
+    await beforeTask();
+    const p = new Promise((res, rej) => {
+      memorix.task.runAlgoNewest
+        .queue("uv6")
+        .then(() => memorix.task.runAlgoNewest.queue("uv7"))
+        .then(({ queueSize }) => {
+          assertEquals(queueSize, 2);
+
+          return new Promise((res) => {
+            const payloads: string[] = [];
+            let stop: any;
+            memorix.task.runAlgoNewest
+              .dequeue((payload) => {
+                payloads.push(payload);
+                if (payloads.length === 2) {
+                  res({
+                    payloads,
+                    stop,
+                  });
+                }
+                return Animal.cat;
+              })
+              .then((args) => {
+                stop = args.stop;
+              });
+          });
+        })
+        .then(async ({ payloads, stop }: any) => {
+          assertEquals(payloads, ["uv7", "uv6"]);
+          await stop();
+          res(undefined);
+        })
+        .catch(rej);
+    });
+    await p;
+  },
+);
+Deno.test("namespace works for cache item", async () => {
+  await memorix.spaceship.cache.pilot.set({ name: "uv" });
+  const pilot = await memorix.spaceship.cache.pilot.get();
+  assertEquals(pilot!.name, "uv");
+});
+Deno.test("namespace works for recursive cache item", async () => {
+  await memorix.spaceship.crew.cache.count.set(10);
+  const count = await memorix.spaceship.crew.cache.count.get();
+  assertEquals(count, 10);
+});
