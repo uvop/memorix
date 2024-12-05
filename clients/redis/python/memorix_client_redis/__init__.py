c8a69db1f19d0c9b0cd4cf48268088266664d731
Yuval Saraf <unimonkiez@gmail.com>
2024-12-05 17:09:24 +0200

    Try again

--- clients/redis/python/memorix_client_redis/__init__.py
+++ clients/redis/python/memorix_client_redis/__init__.py
@@ -3,3 +3,11 @@ from .cache import All as MemorixCacheAll
 from .pubsub import All as MemorixPubSubAll
 from .task import All as MemorixTaskAll
 from dataclasses import dataclass as dataclass
+
+__all__ = [
+    'MemorixBase',
+    'MemorixCacheAll',
+    'MemorixPubSubAll',
+    'MemorixTaskAll',
+    'dataclass'
+]
